98d95b013f832db70bbea5b6da2a8544566932fb
thomasvergne <thomas@quark-lang.dev>
2020-11-12 20:11:07 +0100

    🏷 Node data added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
-module Main where
 import Math
+data Node = Node {
+  program :: String,
+  raw :: String,
+  children :: [Node],
+  parent :: Maybe Node
+}
 
-main :: IO()
+parser :: [Char] -> Int -> Node -> *
+parser code index ast = do
+  if length code == index 
+  then ast 
+  else do
+    print $ code !! index
+    parser code (index + 1) ast
+
 main = do
+  let ast = Node {
+    program = "Program",
+    raw = "",
+    children = [],
+    parent = undefined
+  }
+  let code = "<html><p>test</p><h1>test</h1></html>"
+  parser code 0 ast
   print $ pythagore (3, 4, 5)
