b30ec0673f687871faec6d6db8354e765435bf31
thomasvergne <thomas@quark-lang.dev>
2021-03-03 15:14:38 +0100

    ✅ Complex and optional tests added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,10 @@
 import Complex
+import Optional
 
 main :: IO()
 main = do
-  let complex1 = Complex 1 0
-  let complex2 = Complex 7.4 4
-  let complex3 = Complex 1 (-47)
-  let complex4 = Complex (-4) 7
-  let complex5 = Complex 0 (-4)
+  let test = Ok (Complex 6 6)
 
-  print complex1
-  print complex2
-  print complex3
-  print complex4
-  print complex5
+  print $ getValue test
+  print $ realPart (getValue test)
+
