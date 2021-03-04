d507fc132e93bd8084c379a7a4e1565365913180
thomasvergne <thomas@quark-lang.dev>
2021-03-04 21:53:31 +0100

    ✨ Path join and slice functions added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,27 @@
 import Complex
 import Optional
+import System.Environment
+import System.Directory
+import System.FilePath
+import System.Posix.Directory
+
+argumentParser :: Optional String -> String
+argumentParser Undefined = "tetst"
+argumentParser n = getValue n
+
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = take (to - from + 1) (drop from xs)
+
+join_path :: [[Char]] -> [Char]
+join_path paths = slice 1 (length str) str
+  where str = (foldl (\x y -> x ++ "/" ++ y) "" paths)
 
 main :: IO()
 main = do
-  let test = Ok (Complex 6 6)
+  arguments <- getArgs
+  if length arguments == 0 then
+    putStrLn $ argumentParser Undefined
+  else
+    putStrLn $ argumentParser (Ok (arguments !! 0))
 
-  print $ getValue test
-  print $ realPart (getValue test)
-
-  print $ fmap (++"World") (Ok "Hello ")
+  print $ join_path ["src", "core", "index.js"]
