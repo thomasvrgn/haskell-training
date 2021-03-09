5e6285d67459beb6a62dc9f100f2ea71b0559cea
thomasvergne <thomas@quark-lang.dev>
2021-03-09 22:31:36 +0100

    ✨ From integer function implemented.

--- src/Quaternion.hs
+++ src/Quaternion.hs
@@ -3,6 +3,9 @@ module Quaternion where
 data Quaternion a where
   Quaternion :: (Num a) => a -> a -> a -> a -> Quaternion a
 
+instance (RealFloat a, Num a, Fractional a) => Fractional (Quaternion a) where
+
+
 instance (RealFloat a) => Num (Quaternion a) where
   (Quaternion a b c d) + (Quaternion a' b' c' d') =
     Quaternion (a + a') (b + b') (c + c') (d + d')
@@ -15,6 +18,7 @@ instance (RealFloat a) => Num (Quaternion a) where
       (a * d' + b * c' - c * b' + d * a')
 
   abs z = Quaternion (sqrt (norm z)) 0 0 0
+  fromInteger a = Quaternion (fromInteger a) 0 0 0
 
 norm :: (Num a, Floating a) => Quaternion a -> a
 norm (Quaternion a b c d) = (a**2) + (b**2) + (c**2) + (d**2)
