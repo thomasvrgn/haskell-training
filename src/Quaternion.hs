155bf3375ac69803d9a0f57b5c1378346859ecc0
thomasvergne <thomas@quark-lang.dev>
2021-03-09 22:24:17 +0100

    ✨ Quaternion multiplication and modulus added.

--- src/Quaternion.hs
+++ src/Quaternion.hs
@@ -6,3 +6,20 @@ data Quaternion a where
 instance (RealFloat a) => Num (Quaternion a) where
   (Quaternion a b c d) + (Quaternion a' b' c' d') =
     Quaternion (a + a') (b + b') (c + c') (d + d')
+
+  (Quaternion a b c d) * (Quaternion a' b' c' d') =
+    Quaternion
+      (a * a' - b * b' - c * c' - d * d')
+      (a * b' + b * a' + c * d' - d * c')
+      (a * c' - b * d' + c * a' + d * b')
+      (a * d' + b * c' - c * b' + d * a')
+
+  abs z = Quaternion (sqrt (norm z)) 0 0 0
+
+norm :: (Num a, Floating a) => Quaternion a -> a
+norm (Quaternion a b c d) = (a**2) + (b**2) + (c**2) + (d**2)
+
+-- a => a * a' - b * b' - c * c' - d * d',
+-- bi => a * b' + b * a' + c * d' - d * c',
+-- cj => a * c' - b * d' + c * a' + d * b',
+-- dk => a * d' + b * c' - c * b' + d * a'
