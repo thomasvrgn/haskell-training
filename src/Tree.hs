378c37909830c5934b67a524f57e0fde4b108161
NessMC <contact@nessmc.fr>
2021-03-10 15:04:45 +0100

    ✨ Applicative tree instance added.

--- src/Tree.hs
+++ src/Tree.hs
@@ -4,3 +4,8 @@ module Tree where
   instance Functor Tree where
     fmap f (Item a) = Item (f a)
     fmap f (Node list) = Node (map (fmap f) list)
+
+  instance Applicative Tree where
+    pure = Item
+    (Item list) <*> something = fmap list something
+    (Node list) <*> something = Node [ el <*> something | el <- list ]
