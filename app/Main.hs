11918fa1f71961be8a72cf08d52e139de4a0a86f
thomasvergne <thomas@quark-lang.dev>
2021-03-10 11:01:53 +0100

    🍱 Tree examples added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,42 +1,7 @@
-import Complex
-import Optional
-
-import System.Environment
-import System.Directory
-import System.FilePath
-import System.Posix.Directory
-
-showHelp :: IO String
-showHelp = do
-  putStrLn "Haskell CLI Example"
-  putStrLn " - Version: 0.0.1"
-  putStrLn " - Description: A simple CLI in Haskell\n"
-  return ""
-
-argumentParser :: Optional [String] -> IO String
-argumentParser Undefined = showHelp
-argumentParser n = do
-  putStrLn $ show (getValue n)
-
-  return $ (getValue n) !! 0
-
-slice :: Int -> Int -> [a] -> [a]
-slice from to xs = take (to - from + 1) (drop from xs)
-
-join_path :: [[Char]] -> [Char]
-join_path paths = slice 1 (length str) str
-  where str = (foldl (\x y -> x ++ "/" ++ y) "" paths)
-
-isPrime :: Integer -> Bool
-isPrime n = length (factors n) <= 2
-  where factors n = [ x | x <- [1..n], n `mod` x == 0 ]
+import Tree
 
 main :: IO()
 main = do
-  arguments <- getArgs
-  argumentParser (if length arguments == 0 then Undefined else (Ok arguments))
-  print $ join_path ["src", "core", "index.js"]
-  print $ isPrime 15
-  print $ isPrime 2
-  print $ isPrime 1
-  print $ isPrime 17
+  let tree = Node 4 (Node 5 Leaf)
+  print tree
+  print $ fmap (+5) tree
