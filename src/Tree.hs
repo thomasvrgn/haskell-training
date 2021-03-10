42e41cf441d8a7e02d28923e511f2ce1e15f0ebd
thomasvergne <thomas@quark-lang.dev>
2021-03-10 11:01:43 +0100

    ✨ Tree Functor instance added.

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,2 +1,6 @@
 module Tree where
   data Tree a = Leaf | Node a (Tree a) deriving (Show)
+
+  instance Functor Tree where
+    fmap f Leaf = Leaf
+    fmap f (Node a list) = Node (f a) (fmap f list)
