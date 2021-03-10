b21f6730fe1a3e6b442c787e4729f6950985c5c6
thomasvergne <thomas@quark-lang.dev>
2021-03-10 10:49:04 +0100

    🎨 Quaternion module formatted.

--- src/Quaternion.hs
+++ src/Quaternion.hs
@@ -1,38 +1,38 @@
 {-# LANGUAGE GADTs #-}
 module Quaternion where
-data Quaternion a where
-  Quaternion :: (Num a) => a -> a -> a -> a -> Quaternion a
+  data Quaternion a where
+    Quaternion :: (Num a) => a -> a -> a -> a -> Quaternion a
 
-instance (RealFloat a, Num a, Fractional a) => Fractional (Quaternion a) where
-  (Quaternion a b c d) / z@(Quaternion a' b' c' d') =
-    Quaternion ((a * a' + b * b' + c * c' + d * d') / zNorm)
-               ((-a * b' + b * a' - c * d' + d * c') / zNorm)
-               ((-a * c' + b * d' + c * a' - d * b') / zNorm)
-               ((-a * d' - b * c' + c * b' + d * a') / zNorm)
-    where
-      zNorm = norm z
+  instance (RealFloat a, Num a, Fractional a) => Fractional (Quaternion a) where
+    (Quaternion a b c d) / z@(Quaternion a' b' c' d') =
+      Quaternion ((a * a' + b * b' + c * c' + d * d') / zNorm)
+                 ((-a * b' + b * a' - c * d' + d * c') / zNorm)
+                 ((-a * c' + b * d' + c * a' - d * b') / zNorm)
+                 ((-a * d' - b * c' + c * b' + d * a') / zNorm)
+      where
+        zNorm = norm z
 
-instance (RealFloat a) => Num (Quaternion a) where
-  (Quaternion a b c d) + (Quaternion a' b' c' d') =
-    Quaternion (a + a') (b + b') (c + c') (d + d')
+  instance (RealFloat a) => Num (Quaternion a) where
+    (Quaternion a b c d) + (Quaternion a' b' c' d') =
+      Quaternion (a + a') (b + b') (c + c') (d + d')
 
-  (Quaternion a b c d) - (Quaternion a' b' c' d') =
-    Quaternion (a - a') (b - b') (c - c') (d - d')
+    (Quaternion a b c d) - (Quaternion a' b' c' d') =
+      Quaternion (a - a') (b - b') (c - c') (d - d')
 
-  (Quaternion a b c d) * (Quaternion a' b' c' d') =
-    Quaternion (a * a' - b * b' - c * c' - d * d')
-               (a * b' + b * a' + c * d' - d * c')
-               (a * c' - b * d' + c * a' + d * b')
-               (a * d' + b * c' - c * b' + d * a')
+    (Quaternion a b c d) * (Quaternion a' b' c' d') =
+      Quaternion (a * a' - b * b' - c * c' - d * d')
+                 (a * b' + b * a' + c * d' - d * c')
+                 (a * c' - b * d' + c * a' + d * b')
+                 (a * d' + b * c' - c * b' + d * a')
 
-  abs z = Quaternion (sqrt (norm z)) 0 0 0
-  fromInteger a = Quaternion (fromInteger a) 0 0 0
+    abs z = Quaternion (sqrt (norm z)) 0 0 0
+    fromInteger a = Quaternion (fromInteger a) 0 0 0
 
-norm :: (Num a, Floating a) => Quaternion a -> a
-norm (Quaternion a b c d) = (a**2) + (b**2) + (c**2) + (d**2)
+  norm :: (Num a, Floating a) => Quaternion a -> a
+  norm (Quaternion a b c d) = (a**2) + (b**2) + (c**2) + (d**2)
 
-non :: (Num a) => Quaternion a -> Quaternion a
-non (Quaternion a b c d) = Quaternion a (-b) (-c) (-d)
+  non :: (Num a) => Quaternion a -> Quaternion a
+  non (Quaternion a b c d) = Quaternion a (-b) (-c) (-d)
 
-realPart :: (Num a) => Quaternion a -> a
-realPart (Quaternion a _ _ _) = a
+  realPart :: (Num a) => Quaternion a -> a
+  realPart (Quaternion a _ _ _) = a
