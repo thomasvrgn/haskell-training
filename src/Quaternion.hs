957e835792a7e70f0277fb849563a11655559b29
thomasvergne <thomas@quark-lang.dev>
2021-03-10 10:44:07 +0100

    ✨ Quaternion subtraction and division added.

--- src/Quaternion.hs
+++ src/Quaternion.hs
@@ -4,11 +4,20 @@ data Quaternion a where
   Quaternion :: (Num a) => a -> a -> a -> a -> Quaternion a
 
 instance (RealFloat a, Num a, Fractional a) => Fractional (Quaternion a) where
-
+  (Quaternion a b c d) / z@(Quaternion a' b' c' d') =
+    Quaternion ((a * a' + b * b' + c * c' + d * d') / zNorm)
+               ((-a * b' + b * a' - c * d' + d * c') / zNorm)
+               ((-a * c' + b * d' + c * a' - d * b') / zNorm)
+               ((-a * d' - b * c' + c * b' + d * a') / zNorm)
+    where
+      zNorm = norm z
 
 instance (RealFloat a) => Num (Quaternion a) where
   (Quaternion a b c d) + (Quaternion a' b' c' d') =
     Quaternion (a + a') (b + b') (c + c') (d + d')
+
+  (Quaternion a b c d) - (Quaternion a' b' c' d') =
+    Quaternion (a - a') (b - b') (c - c') (d - d')
 
   (Quaternion a b c d) * (Quaternion a' b' c' d') =
     Quaternion
@@ -26,7 +35,5 @@ norm (Quaternion a b c d) = (a**2) + (b**2) + (c**2) + (d**2)
 non :: (Num a) => Quaternion a -> Quaternion a
 non (Quaternion a b c d) = Quaternion a (-b) (-c) (-d)
 
--- a => a * a' - b * b' - c * c' - d * d',
--- bi => a * b' + b * a' + c * d' - d * c',
--- cj => a * c' - b * d' + c * a' + d * b',
--- dk => a * d' + b * c' - c * b' + d * a'
+realPart :: (Num a) => Quaternion a -> a
+realPart (Quaternion a _ _ _) = a
