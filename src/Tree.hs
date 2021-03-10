01a2e774029236193b7cf046598efc3842c15ca7
thomasvergne <thomas@quark-lang.dev>
2021-03-10 19:30:13 +0100

    💬 Item replaced with Leaf.

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,11 +1,11 @@
 module Tree where
-  data Tree a = Node [(Tree a)] | Item a deriving (Show)
+  data Tree a = Node [(Tree a)] | Leaf a deriving (Show)
 
   instance Functor Tree where
-    fmap f (Item a) = Item (f a)
+    fmap f (Leaf a) = Leaf (f a)
     fmap f (Node list) = Node (map (fmap f) list)
 
   instance Applicative Tree where
-    pure = Item
-    (Item list) <*> something = fmap list something
+    pure = Leaf
+    (Leaf list) <*> something = fmap list something
     (Node list) <*> something = Node [ el <*> something | el <- list ]
