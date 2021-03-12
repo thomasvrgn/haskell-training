97017cb053d22a2aea170f0324b0eee1b440908e
thomasvergne <thomas@quark-lang.dev>
2021-03-12 17:53:45 +0100

    🍱 Quaternion tree example added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 import Tree
+import Pythagore
+import Quaternion
 
 main :: IO()
 main = do
-  let tree = Node [Leaf 5, Node [Leaf 1]]
+  let tree = Node [Leaf (Quaternion 7 5 6 4), Node [Leaf (Quaternion 0 1 2 3), Leaf (Quaternion 4 5 6 7)]]
   print tree
-  print $ tree >>= \x -> return (x + 5)
-  print $ (Leaf 5) >>> (+5)
-  print $ tree >>> (+5)
+  print $ (+5) <$> tree
