44df33996e2eb99b61f8974af476e1f0b2ef5c3d
thomasvergne <thomas@quark-lang.dev>
2021-03-12 21:59:19 +0100

    🍱 Exponential and functor examples added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 import Tree
-import Pythagore
 import Quaternion
 
 main :: IO()
 main = do
   let tree = Node [Leaf (Quaternion 7 5 6 4), Node [Leaf (Quaternion 0 1 2 3), Leaf (Quaternion 4 5 6 7)]]
   print tree
-  print $ (+5) <$> tree
+  print $ pure exp <*> pure (Quaternion 1 2 3 4) <$> tree
+  print $ exp (Quaternion 0 1 2 3)
+  print $ (+5) <$> (Quaternion 0 1 2 3)
