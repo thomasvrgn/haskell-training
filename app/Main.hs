12905271ef1437395f4ee291a20454678f1ef896
NessMC <contact@nessmc.fr>
2021-03-14 15:55:30 +0100

    ✨ Square root Newton method function added.

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,12 @@ import Tree
 import Quaternion
 import Addition
 import Product
+import Control.Monad (unless)
+
+sqrt' :: (Num a, Eq a, Floating a) => a -> a -> a
+sqrt' dec 0 = 1
+sqrt' dec x = x1 - ((x1**2 - dec) / (2*x1))
+  where x1 = sqrt' dec (x - 1)
 
 evalPolish :: (Num a, Read a, Fractional a) => String -> a
 evalPolish list = (head . foldl processItem [] . words) list
@@ -14,3 +20,4 @@ evalPolish list = (head . foldl processItem [] . words) list
 main = do
   print "test"
   print $ Product 4 * Product 5
+  print $ sqrt' 10 2
