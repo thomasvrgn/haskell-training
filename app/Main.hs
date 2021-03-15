9b325229cb87bedeeaf6579120dfa8c70e33d61f
thomasvergne <thomas@quark-lang.dev>
2021-03-15 20:45:08 +0100

    💩 Bad square root function added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,16 @@
 import Tree
-import Quaternion
-import Addition
-import Product
+import Complex.Quaternion
+import Monoids.Addition
+import Monoids.Product
 import Control.Monad (unless)
 
 sqrt' :: (Num a, Eq a, Floating a) => a -> a -> a
 sqrt' dec 0 = 1
 sqrt' dec x = x1 - ((x1**2 - dec) / (2*x1))
   where x1 = sqrt' dec (x - 1)
+
+squareroot :: (Num a, Floating a, Eq a) => a -> a
+squareroot x = sqrt' x 10
 
 evalPolish :: (Num a, Read a, Fractional a) => String -> a
 evalPolish list = (head . foldl processItem [] . words) list
@@ -20,4 +23,4 @@ evalPolish list = (head . foldl processItem [] . words) list
 main = do
   print "test"
   print $ Product 4 * Product 5
-  print $ sqrt' 10 2
+  mapM_ (\x -> putStrLn (show x ++ ": " ++ show (squareroot x))) [1..10]
