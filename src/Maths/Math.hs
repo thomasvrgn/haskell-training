b51ac8a56b0905cb07025485b5e8f0d0260e4e8e
thomasvergne <thomas@quark-lang.dev>
2021-03-19 22:18:34 +0100

    🚚 Math related functions moved to Math module.

--- src/Maths/Math.hs
+++ src/Maths/Math.hs
@@ -3,3 +3,19 @@ module Maths.Math (
 ) where
 
 import Maths.Pythagore
+
+sqrt' :: (Num a, Eq a, Floating a) => a -> a -> a
+sqrt' dec 0 = 1
+sqrt' dec x = x1 - ((x1**2 - dec) / (2*x1))
+  where x1 = sqrt' dec (x - 1)
+
+evalPolish :: (Num a, Read a, Fractional a) => String -> a
+evalPolish list = (head . foldl processItem [] . words) list
+  where processItem (x:y:ys) "+" = (x + y):ys
+        processItem (x:y:ys) "-" = (y - x):ys
+        processItem (x:y:ys) "*" = (x * y):ys
+        processItem (x:y:ys) "/" = (x / y):ys
+        processItem xs number = read number:xs
+
+hyperPower :: Integer -> Integer
+hyperPower x = foldl (\x y -> (x * x)) x [1..x]
