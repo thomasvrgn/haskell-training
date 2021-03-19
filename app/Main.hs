53bd9f77f01506e7d8c30b67b5344b7f0ffb603c
thomasvergne <thomas@quark-lang.dev>
2021-03-19 23:28:26 +0100

    🍱 Quaternion monad example added.

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,11 @@ import Control.Monad (unless)
 import Semigroups.AntiCommutative
 import Control.Monad.State.Lazy
 
+qtprocess :: Num a => Quaternion a
+qtprocess = do
+  qt <- Quaternion 0 1 2 3
+  return $ qt *2
+
 main :: IO()
 main = do
-  print "test"
+  print $ qtprocess
