d3f48a0b684ad785d1a5db9ac09999289c1d272c
thomasvergne <thomas@quark-lang.dev>
2021-03-19 23:21:41 +0100

    ✨ Functor, Applicative and Monad Quaternion instances added.

--- src/Complex/Quaternion.hs
+++ src/Complex/Quaternion.hs
@@ -1,10 +1,23 @@
 {-# LANGUAGE GADTs, StandaloneDeriving #-}
 module Complex.Quaternion where
-  data Quaternion a where
-    Quaternion :: (Num a) => a -> a -> a -> a -> Quaternion a
+  data Quaternion a = Quaternion a a a a
 
   deriving instance (Show a) => Show (Quaternion a)
   deriving instance (Eq a) => Eq (Quaternion a)
+
+  instance Functor Quaternion where
+    fmap f (Quaternion a b c d) = Quaternion (f a) (f b) (f c) (f d)
+
+  instance Applicative Quaternion where
+    pure x = Quaternion x x x x
+    (Quaternion f g h j) <*> (Quaternion a b c d) = Quaternion (f a) (g b) (h c) (j d)
+
+  instance Monad Quaternion where
+    (Quaternion a b c d) >>= f = Quaternion a' b' c' d'
+      where Quaternion a' _ _ _ = f a
+            Quaternion _ b' _ _ = f b
+            Quaternion _ _ c' _ = f c
+            Quaternion _ _ _ d' = f d
 
   instance (RealFloat a, Num a, Fractional a) => Fractional (Quaternion a) where
     (Quaternion a b c d) / z@(Quaternion a' b' c' d') =
@@ -51,5 +64,11 @@ module Complex.Quaternion where
   non :: (Num a) => Quaternion a -> Quaternion a
   non (Quaternion a b c d) = Quaternion a (-b) (-c) (-d)
 
-  realPart :: (Num a) => Quaternion a -> a
+  realPart :: Quaternion a -> a
   realPart (Quaternion a _ _ _) = a
+
+  imagPart :: Quaternion a -> Int -> a
+  imagPart (Quaternion _ a b c) y = case y of
+    0 -> a
+    1 -> b
+    2 -> c
