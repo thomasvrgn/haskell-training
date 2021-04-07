ac6231e71696f87c32f3d392e46c7e5b7c48d5b3
thomasvergne <thomas@quark-lang.dev>
2021-04-07 11:23:24 +0200

    🍱 Simple mutable example added.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,34 @@
+{-# LANGUAGE BlockArguments, LambdaCase #-}
 module Main where
+  import Mutable
+
+  popLast :: [a] -> [a]
+  popLast xs = take ((length xs) - 1) xs
+
+  type Stack = [String]
+
+  empty :: Stack
+  empty = []
+
+  unshift :: Mutable Stack String
+  unshift = Mutable \case
+    [] -> ("", [])
+    (x:xs) -> (x, xs)
+
+  pop :: Mutable Stack String
+  pop = Mutable \case
+    [] -> ("", [])
+    xs -> (last xs, popLast xs)
+
+  push :: String -> Mutable Stack ()
+  push str = Mutable \s -> ((), s ++ [str])
+
+  stackTest :: String -> Mutable Stack ()
+  stackTest user = do
+    push "Hello"
+    push user
+
   main :: IO()
-  main = print "test"
+  main = do
+    let mutTest = runMutable (stackTest "Thomas") empty
+    print $ mutTest
