68a9c21f33a765f9a33fec9de94f7c3185f824ea
thomasvergne <thomas@quark-lang.dev>
2021-04-07 10:53:24 +0200

    ✨ Mutable Applicative instance added.

--- src/Mutable.hs
+++ src/Mutable.hs
@@ -7,3 +7,11 @@ module Mutable where
     fmap f (Mutable mut) = Mutable \s ->
       let (a, s') = mut s
         in (f a, s')
+
+  instance Applicative (Mutable s) where
+    pure x = Mutable \s -> (x, s)
+
+    (Mutable mutf) <*> (Mutable mut) = Mutable \s ->
+      let (f, s') = mutf s
+          (a, b) = mut s'
+        in (f a, b)
