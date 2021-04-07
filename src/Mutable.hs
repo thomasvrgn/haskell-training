7f032142795367c945ae77ae0df3dae95e802ad9
thomasvergne <thomas@quark-lang.dev>
2021-04-07 10:51:21 +0200

    ✨ Mutable Functor instance added.

--- src/Mutable.hs
+++ src/Mutable.hs
@@ -1,2 +1,9 @@
+{-# LANGUAGE BlockArguments #-}
 module Mutable where
   newtype Mutable s a = Mutable { runState :: s -> (a, s) }
+
+  -- We don't want to change state output but only the result
+  instance Functor (Mutable s) where
+    fmap f (Mutable mut) = Mutable \s ->
+      let (a, s') = mut s
+        in (f a, s')
