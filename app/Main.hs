576ad56af141f9aa1cc49d5cc7ac3cd196907da9
thomasvergne <thomas@quark-lang.dev>
2021-04-07 14:37:17 +0200

    ⚡ Neper function added.

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 module Main where
   import Data.List
   import Mutable
+  import Optional
 
   popLast :: [a] -> [a]
   popLast xs = take ((length xs) - 1) xs
@@ -36,7 +37,16 @@ module Main where
   makeSentence :: [String] -> String
   makeSentence xs = concat . intersperse " " $ xs
 
+  factorial :: (Num a, Enum a, Eq a) => a -> a
+  factorial 0 = 1
+  factorial n = product [1..n]
+
+  neper :: (Num a, Enum a, Eq a, Fractional a) => Optional a -> a
+  neper (Ok n) = foldl (\x acc -> x + (1 / factorial acc)) 1 [1..n]
+  neper Undefined = neper (Ok 20)
+
   main :: IO()
   main = do
     let mutTest = getMutable (stackTest "Thomas") empty
     putStrLn $ makeSentence mutTest
+    print $ neper Undefined
