57468d432cc99ee8af0e1e55183138fc35f624ae
thomasvergne <thomas@quark-lang.dev>
2021-04-13 20:19:19 +0100

    ⚡ Tree functor instance optimized.

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,14 +1,17 @@
 module Tree where
-  data Tree a = Node [(Tree a)] | Leaf a deriving (Show)
+  data Tree a
+    = Node [Tree a]
+    | Leaf a
+    deriving (Show)
 
   instance Functor Tree where
-    fmap f (Leaf a) = Leaf (f a)
-    fmap f (Node list) = Node (map (fmap f) list)
+    fmap f (Leaf a) = Leaf $ f a
+    fmap f (Node list) = Node $ map (fmap f) list
 
   instance Applicative Tree where
     pure = Leaf
-    (Leaf list) <*> something = fmap list something
-    (Node list) <*> something = Node [ el <*> something | el <- list ]
+    (Leaf f) <*> something = f <$> something
+    (Node xs) <*> something = Node [ f <*> something | f <- xs ]
 
   instance Monad Tree where
     return = Leaf
